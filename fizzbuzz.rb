e43b270c4ac25ac83d7cbf3ebf31dc09e35f6cae
Ben Griffiths <bengriffiths@gmail.com>
2017-09-10 12:20:03 +0100

    FizzBuzzTests#test_fizzbuzz

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,4 +1,7 @@
 class FizzBuzzer
+  def for_number(i)
+    "FizzBuzz"
+  end
   def for_number_case(num)
     case
     when ((num % 3) == 0)
--- tests.rb
+++ tests.rb
@@ -4,6 +4,9 @@ class FizzBuzzTests < Test::Unit::TestCase
   def setup
     @fizzbuzzer = FizzBuzzer.new
   end
+  def test_fizzbuzz
+    assert_equal("FizzBuzz", @fizzbuzzer.for_number(15))
+  end
   def test_fizz_c
     assert_equal("Fizz", @fizzbuzzer.for_number_case(3))
   end
